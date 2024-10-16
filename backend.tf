6142626de198ea11b7fc8bf1748197898c0f0957
olusola <sholex111@gmail.com>
2024-10-16 19:10:32 +0000

    backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = "kaydemo01"  # Use the static name of the state bucket
-#     prefix = "terraform/state"
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket = "kaydemo02"  # Use the static name of the state bucket
+    prefix = "terraform/state"
+  }
+}
